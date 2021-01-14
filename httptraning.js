81d615a4305b22ccd9edb1144c42aadf8c087f49
yasir-invozone <yasir.mehboob@invozone.com>
2021-01-14 12:59:42 +0500

    make routes commit

--- httptraning.js
+++ httptraning.js
@@ -1,6 +1,14 @@
 const http = require('http');
+const url = require('url');
 const server = http.createServer((req, res) => {
-    res.end('hello i am sending you some responce angainst request');
+    if(req.url == '/'){
+        res.end('hello i am sending you some responce from homepage');
+    } else if(req.url == '/about'){
+        res.end('hello i am sending you some responce from about page');
+    } else {
+        res.writeHead(404);
+        res.end('404 Page not found');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
